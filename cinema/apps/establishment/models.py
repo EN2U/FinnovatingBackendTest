05f20e67293db259a5d330a41c651641750dd00d
Eduardo Nuez Wehbe <nuezwehbe@gmail.com>
2022-07-26 12:44:54 +0100

    Create Establishment model fields

--- cinema/apps/establishment/models.py
+++ cinema/apps/establishment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Establishment(models.Model):
+    direction = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    rating = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
