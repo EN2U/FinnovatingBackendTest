fe88803e0a6c2dca4ab83638cefcc9c73143406f
Eduardo Nuez Wehbe <nuezwehbe@gmail.com>
2022-07-26 17:07:08 +0100

    Add filtering by establishment name on schema

--- cinema/apps/establishment/schema.py
+++ cinema/apps/establishment/schema.py
@@ -22,8 +22,15 @@ class EstablishmentType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_establishment = graphene.List(EstablishmentType)
 
+    establishment_by_name = graphene.List(
+        EstablishmentType, name=graphene.String(required=True)
+    )
+
     def resolve_all_establishment(root, info):
         return Establishment.objects.all()
+
+    def resolve_establishment_by_name(root, info, name):
+        return Establishment.objects.filter(name__icontains=name)
 
 
 schema = graphene.Schema(query=Query)
