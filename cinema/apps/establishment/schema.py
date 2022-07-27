8746a0ddfb0ea351ec252ea8eb2eb44441c5d2ab
Eduardo Nuez Wehbe <nuezwehbe@gmail.com>
2022-07-27 11:15:08 +0100

    Add establishment mutation to update establishment

--- cinema/apps/establishment/schema.py
+++ cinema/apps/establishment/schema.py
@@ -5,6 +5,7 @@ from .models import Establishment
 
 
 class EstablishmentInput(graphene.InputObjectType):
+    id = graphene.ID()
     direction = graphene.String()
     name = graphene.String()
     rating = graphene.Int()
@@ -62,5 +63,27 @@ class CreateEstablishment(graphene.Mutation):
         return CreateEstablishment(establishment=establishment_instance)
 
 
+class UpdateEstablishment(graphene.Mutation):
+    class Arguments:
+        establishment_data = EstablishmentInput(required=True)
+
+    establishment = graphene.Field(EstablishmentType)
+
+    def mutate(self, info, establishment_data=None):
+        establishment_instance = Establishment.objects.get(
+            pk=establishment_data.id
+        )
+
+        if establishment_data:
+
+            for key, value in establishment_data.items():
+                setattr(establishment_instance, key, value)
+
+            establishment_instance.save()
+            return UpdateEstablishment(establishment=establishment_instance)
+        return UpdateEstablishment(establishment=None)
+
+
 class Mutation(graphene.ObjectType):
     create_establishment = CreateEstablishment.Field()
+    update_establishment = UpdateEstablishment.Field()
